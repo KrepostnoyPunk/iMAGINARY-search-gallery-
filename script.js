1985e324d48ef1c40a47c17b771fe4345b15b935
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2024-01-23 21:01:11 +0300

    Need to complete JS part.

---
+++ script.js
@@ -0,0 +1,12 @@
+const keyAPI="C5gygLuETBkmpxd36JT1IrVDpbHhDalMX6IxPMQ_xbw"
+const formEl=document.querySelector('form')
+const searchInputEl=document.getElementById("search-input")
+const searchResultsEl=document.querySelector(".search-results")
+//const searchBtnEl=document.getElementById("search-btn")
+const showMoreBtnEl=document.getElementById("show-more")
+
+
+formEl.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    //console.log('subm');
+})
