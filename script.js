b1f638122aaca3697a872cb4ebb393551e6a1471
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2024-01-29 21:01:43 +0300

    Done.

--- script.js
+++ script.js
@@ -6,7 +6,60 @@ const searchResultsEl=document.querySelector(".search-results")
 const showMoreBtnEl=document.getElementById("show-more")
 
 
+let inputData="";
+let page=1;
+
+
+async function searchImages(){
+    inputData=searchInputEl.value
+    //console.log(inputData);
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${keyAPI}`
+    //console.log(url);
+    const response=await fetch(url)
+    const data=await response.json()
+    //console.log(data);
+    if(page===1){
+        searchResultsEl.innerHTML=""
+    }
+    const results=data.results 
+    results.map((result)=>{
+        const imageWrapper=document.createElement('div')
+        imageWrapper.classList.add('search-result')
+
+        const image=document.createElement("img")
+
+        image.src=result.urls.small
+        image.alt=result.alt_description
+
+        const imageLink=document.createElement("a")
+
+        imageLink.href=result.links.html 
+        imageLink.target="_blank"
+        imageLink.textContent=result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+
+        searchResultsEl.appendChild(imageWrapper)
+    })
+
+    page++
+    
+    if(page>1){
+        showMoreBtnEl.style.display="block"
+    }
+    //console.log(result);
+}
+
+
 formEl.addEventListener('submit', (event)=>{
     event.preventDefault()
     //console.log('subm');
+    page=1;
+    searchImages()
+})
+
+
+showMoreBtnEl.addEventListener("click", ()=>{
+    searchImages()
 })
